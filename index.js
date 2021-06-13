dd5a1bafded25501533ed634bb71e3b6ee9a6100
parveen-sishodiya-bitbucket <parveen.sishodiya.official@gmail.com>
2021-06-13 13:39:58 +0530

    added icons

--- index.js
+++ index.js
@@ -1,7 +1,132 @@
 const app = angular.module("app", []);
 app.controller("wnwdController", function ($scope) {
 
-    let toplatinner = 36.555554965543216;
+
+    $scope.title = "Windward"
+    
+    var map = L.map('map').setView([22.805, 82.0], 5);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoicGFydmVlbm1hcCIsImEiOiJja2ttc2trdGwzYmNoMm9wYTdsNnZmYTdkIn0.I0PS604HIje1rmMpkHKTiQ'
+    }).addTo(map);
+
+    var markerLayer = L.canvasIconLayer({}).addTo(map);
+
+    
+    let greenIcon = L.icon({
+        iconUrl: "./assets/img/icon/1tgreen.png",
+        iconSize: [20, 20],
+        iconAnchor: [10, 9]
+    });
+
+    let redIcon = L.icon({
+        iconUrl: "./assets/img/icon/1tred.png",
+        iconSize: [20, 20],
+        iconAnchor: [10, 9]
+    });
+
+    let purpleIcon = L.icon({
+        iconUrl: "./assets/img/icon/1tpurple.png",
+        iconSize: [20, 20],
+        iconAnchor: [10, 9],
+        className: 'blinker'
+    });
+
+    let brownIcon = L.icon({
+        iconUrl: "./assets/img/icon/1tbrown.png",
+        iconSize: [20,20],
+        iconAnchor: [10, 9]
+    });
+
+    var gicon = L.icon({
+        iconUrl: 'assets/img/icon/1tbrown.png',
+        iconSize: [20, 20],
+        iconAnchor: [10, 9]
+      });
+
+
+
+    let markers = [];
+
+    for (i = 0; i < 20000; i++) {
+        let latlongs = [];
+        latlongs.push(getRandomInRange(-180, 180, 3))
+        latlongs.push(getRandomInRange(-180, 180, 3))
+        let marker =  L.marker(latlongs,{
+            icon:getIcon()
+        }).bindPopup("I Am "+i);
+        markers.push(marker);   
+    }
+
+    markerLayer.addLayers(markers);
+
+    $scope.showMarkerLayer= ()=>{
+        map.addLayer(markerLayer);
+    }
+
+    $scope.hideMarkerLayer= ()=>{
+        map.removeLayer(markerLayer);
+    }
+
+    function getIcon() {
+        let icon = Math.random() * 10;
+        if (icon <= 1) {
+            return greenIcon;
+        }
+        if (icon <= 2) {
+            return brownIcon;
+        }
+
+        if (icon <= 3) {
+            return purpleIcon;
+        }
+        return redIcon;
+    }
+
+    function getIconUrl() {
+        let icon = Math.random() * 10;
+        if (icon <= 1) {
+            return "./assets/img/icon/1tgreen.png";
+        }
+        if (icon <= 2) {
+            return "./assets/img/icon/1tbrown.png";
+        }
+
+        if (icon <= 3) {
+            return "./assets/img/icon/1tpurple.png";
+        }
+        return "./assets/img/icon/1tred.png";
+    }
+
+    function getIconColor() {
+        let icon = Math.random() * 10;
+        if (icon <= 1) {
+            return "green";
+        }
+        if (icon <= 2) {
+            return "brown";
+        }
+
+        if (icon <= 3) {
+            return "purple";
+        }
+        return "red";
+    }
+
+   
+    function getRandomInRange(from, to, fixed) {
+        return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+        // .toFixed() returns string, so ' * 1' is a trick to convert to number
+    }
+
+    //vanila code
+    /*
+     let toplatinner = 36.555554965543216;
     let rightlonginner = 115.7871101796627;
     let bottomlatinner = 7.57163356840944;
     let leftlonginner = 48.28711017966271;
@@ -10,9 +135,7 @@ app.controller("wnwdController", function ($scope) {
     $scope.title = "Windward"
     var map = L.map('map').setView([22.805, 82.0], 5);
 
-    var myRenderer = L.canvas({ padding: 0.5 });
-
-    //var myRenderer = L.canvas({ padding: 0.5 });
+    
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -36,13 +159,13 @@ app.controller("wnwdController", function ($scope) {
         rightlonginner = bounds['_northEast']['lng'];
         bottomlatinner = bounds['_southWest']['lat'];
         leftlonginner = bounds['_southWest']['lng'];
-        plotIconsOnLoad();
-        /* now send your bounds to the server, requesting only the visible markers */
+        //plotIconsOnLoad();
     })
 
     let layer = L.layerGroup();
     map.addLayer(layer);
 
+    
     let markers = [];
     let marker;
 
@@ -126,10 +249,6 @@ app.controller("wnwdController", function ($scope) {
         return "red";
     }
 
-    // setInterval(() => {
-    //     plotIconsOnLoad();
-    // }, 1000);
-
     function getRandomInRange(from, to, fixed) {
         return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
         // .toFixed() returns string, so ' * 1' is a trick to convert to number
@@ -177,5 +296,8 @@ app.controller("wnwdController", function ($scope) {
 
     map.on('load', plotIconsOnLoad())
 
-    // map.on('move', plotIconsOnLoad)
+     map.on('move', plotIconsOnLoad)
+    */
+
+
 })
